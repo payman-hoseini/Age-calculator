225098e8c188a0a35872df307ee127fa9de872d4
Payman <payman.hoseini7@gmail.com>
2024-09-02 17:28:15 +0430

    prepare libraries

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,17 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors : {
+        'Purple' : 'hsl(259, 100%, 65%)',
+        'Light-red' : 'hsl(0, 100%, 67%)',
+        'Off-white' : 'hsl(0, 0%, 94%)',
+        'Light-grey' : 'hsl(0, 0%, 86%)',
+        'Smokey-grey' : 'hsl(0, 1%, 44%)',
+        'Off-black' : 'hsl(0, 0%, 8%)',
+      },
+      fontFamily : {
+        Poppins : ['Poppins']
+      }
     },
   },
   plugins: [],
