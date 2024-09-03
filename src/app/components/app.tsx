5a7610606146991619cf3dbdeb2c62f2f9ad7d4c
Payman <payman.hoseini7@gmail.com>
2024-09-03 22:36:02 +0430

    90%

--- src/app/components/app.tsx
+++ src/app/components/app.tsx
@@ -9,16 +9,19 @@ export default function App(){
     const [month,setMonth] = useState<number>();
     const [year,setYear] = useState<number>();
 
+    const [dayAge,setDayAge] = useState<number>(0);
+    const [monthAge,setMonthAge] = useState<number>(0);
+    const [yearAge,setYearAge] = useState<number>(0);
     return (
         <main className="bg-Off-white h-screen flex justify-center items-center font-Poppins">
-            <div className="bg-white max-w-4xl p-10 rounded-t-xl rounded-bl-xl rounded-br-[200px]">
+            <div className="bg-white max-w-4xl w-[90vw] p-10 rounded-t-xl rounded-bl-xl rounded-br-[200px]">
                 <div>
-                    <Form day={day} setDay={setDay} month={month} setMonth={setMonth} year={year} setYear={setYear}/>
+                    <Form day={day} setDay={setDay} month={month} setMonth={setMonth} year={year} setYear={setYear} setDayAge={setDayAge} setMonthAge={setMonthAge} setYearAge={setYearAge}/>
                 </div>
                 <div className="mb-3">
-                    <p className="text-8xl text-Off-black font-bold italic"><span className="text-Purple">- -</span> years</p>
-                    <p className="text-8xl text-Off-black font-bold italic mt-2"><span className="text-Purple">- -</span> months</p>
-                    <p className="text-8xl text-Off-black font-bold italic mt-2"><span className="text-Purple">- -</span> days</p>
+                    <p className="text-8xl text-Off-black font-bold italic"><span className="text-Purple">{(yearAge == 0) ? '- -' : yearAge}</span> years</p>
+                    <p className="text-8xl text-Off-black font-bold italic mt-2"><span className="text-Purple">{(monthAge == 0) ? '- -' : monthAge}</span> months</p>
+                    <p className="text-8xl text-Off-black font-bold italic mt-2"><span className="text-Purple">{(dayAge == 0) ? '- -' : dayAge}</span> days</p>
                 </div>
             </div>
         </main>
--- src/app/components/form.tsx
+++ src/app/components/form.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 
-export default function Form({day , setDay , month , setMonth , year , setYear} : any){
+export default function Form({day , setDay , month , setMonth , year , setYear , setDayAge , setMonthAge , setYearAge} : any){
 
     const [emptyDayErr , setEmptyDayErr] = useState(false)
     const [emptyMonthErr , setEmptyMonthErr] = useState(false)
@@ -23,23 +23,71 @@ export default function Form({day , setDay , month , setMonth , year , setYear} 
 
     function calculateAge (){
         //empty inputs
-        (day == undefined || day == '') ? setEmptyDayErr(true) : setEmptyDayErr(false);
-        (month == undefined || month == '') ? setEmptyMonthErr(true) : setEmptyMonthErr(false);
-        (year == undefined || year == '') ? setEmptyYearErr(true) : setEmptyYearErr(false);
+        let emptyDayFlag = day == undefined || day == '';
+        let emptyMonthFlag = month == undefined || month == '';
+        let emptyYearFlag = year == undefined || year == '';
+        let wrongDayFlag =  day < 0 || day > 31;
+        let wrongMonthFlag =  month <= 0 || month > 12;
+        let wrongYearFlag =  year <= 0 || year > (new Date().getFullYear());
+
+        (emptyDayFlag) ? setEmptyDayErr(true) : setEmptyDayErr(false);
+        (emptyMonthFlag) ? setEmptyMonthErr(true) : setEmptyMonthErr(false);
+        (emptyYearFlag) ? setEmptyYearErr(true) : setEmptyYearErr(false);
         // wrong inputs
-        ( day < 0 || day > 31) ? setWrongDay(true) : setWrongDay(false);
-        (month <= 0 || month > 12) ? setWrongMonth(true) : setWrongMonth(false);
-        (year <= 0 || year > (new Date().getFullYear())) ? setWrongYear(true) : setWrongYear(false);
-        console.log(day)
+        (wrongDayFlag) ? setWrongDay(true) : setWrongDay(false);
+        (wrongMonthFlag) ? setWrongMonth(true) : setWrongMonth(false);
+        (wrongYearFlag) ? setWrongYear(true) : setWrongYear(false);
+
+        // calculate age
+        let now = new Date();
+        let currentYear = now.getFullYear()
+        let currentMonth = now.getMonth()
+        let currentDay = now.getDate()
+        let yearAge;
+        let monthAge;
+        let dayAge;
+
+        yearAge = currentYear - year;
+
+        if(currentMonth >= month){
+            monthAge = currentMonth - month
+        }
+        else{
+            yearAge--;
+            monthAge = 12 + (currentMonth - month)
+        }
+
+        if(currentDay >= day){
+            dayAge = currentDay - day
+        }
+        else{
+            monthAge--;
+            dayAge = 31 + (currentDay - day);
+            if(monthAge < 0){
+                monthAge = 1;
+                yearAge--;
+            }
+        }
+        if(emptyDayFlag || emptyMonthFlag || emptyYearFlag || wrongDayFlag || wrongMonthFlag || wrongYearFlag){
+            setDayAge(0)
+            setMonthAge(0);
+            setYearAge(0);
+        }
+        else{
+            setDayAge(dayAge)
+            setMonthAge(monthAge);
+            setYearAge(yearAge);
+        }
     }
 
 
+    // console.log(miladi_be_shamsi(new Date().getFullYear() , new Date().getMonth() , new Date().getDate()))
     return (
         <>
             <form action="#" className="flex w-[80%]">
                 <div className="flex flex-col">
-                    <label htmlFor="day" className={`text-Smokey-grey tracking-widest ${(emptyDayErr || wrongDay) ? 'text-Light-red' : ''}`}>DAY</label>
-                    <input onChange={dayChangeHandler} className={`mt-1 text-2xl rounded-lg border-2 border-Off-white w-[75%] p-4 focus:outline-Purple ${(emptyDayErr || wrongDay) ? 'border-Light-red' : ''}`} type="number" name="day" placeholder="DD"/>
+                    <label htmlFor="day" className={` tracking-widest ${(emptyDayErr || wrongDay) ? 'text-Light-red' : 'text-Smokey-grey'}`}>DAY</label>
+                    <input onChange={dayChangeHandler} className={`mt-1 text-2xl rounded-lg border-2 w-[75%] p-4 focus:outline-Purple ${(emptyDayErr || wrongDay) ? 'border-Light-red' : 'border-Off-white'}`} type="number" name="day" placeholder="DD"/>
                     {
                         emptyDayErr && <span className="mt-2 italic text-sm text-Light-red">This field is requierd</span>
                     }
@@ -48,8 +96,8 @@ export default function Form({day , setDay , month , setMonth , year , setYear} 
                     }
                 </div>
                 <div className="flex flex-col">
-                    <label htmlFor="" className={`text-Smokey-grey tracking-widest ${(emptyMonthErr || wrongMonth) ? 'text-Light-red' : ''}`}>MONTH</label>
-                    <input onChange={monthChangeHandler} className={`mt-1 text-2xl rounded-lg border-2 border-Off-white w-[75%] p-4 focus:outline-Purple ${(emptyMonthErr || wrongMonth) ? 'border-Light-red' : ''}`} type="number" placeholder="MM"/>
+                    <label htmlFor="" className={` tracking-widest ${(emptyMonthErr || wrongMonth) ? 'text-Light-red' : 'text-Smokey-grey'}`}>MONTH</label>
+                    <input onChange={monthChangeHandler} className={`mt-1 text-2xl rounded-lg border-2 w-[75%] p-4 focus:outline-Purple ${(emptyMonthErr || wrongMonth) ? 'border-Light-red' : 'border-Off-white'}`} type="number" placeholder="MM"/>
                     {
                         emptyMonthErr && <span className="mt-2 italic text-sm text-Light-red">This field is requierd</span>
                     }
@@ -58,8 +106,8 @@ export default function Form({day , setDay , month , setMonth , year , setYear} 
                     }
                 </div>
                 <div className="flex flex-col">
-                    <label htmlFor="" className={`text-Smokey-grey tracking-widest ${(emptyYearErr || wrongYear) ? 'text-Light-red' : ''}`}>YEAR</label>
-                    <input onChange={yearChangeHandler} className={`mt-1 text-2xl rounded-lg border-2 border-Off-white w-[75%] p-4 focus:outline-Purple ${(emptyYearErr || wrongYear) ? 'border-Light-red' : ''}`} type="number" placeholder="YYYY"/>
+                    <label htmlFor="" className={` tracking-widest ${(emptyYearErr || wrongYear) ? 'text-Light-red' : 'text-Smokey-grey'}`}>YEAR</label>
+                    <input onChange={yearChangeHandler} className={`mt-1 text-2xl rounded-lg border-2 w-[75%] p-4 focus:outline-Purple ${(emptyYearErr || wrongYear) ? 'border-Light-red' : 'border-Off-white'}`} type="number" placeholder="YYYY"/>
                     {
                         emptyYearErr && <span className="mt-2 italic text-sm text-Light-red">This field is requierd</span>
                     }
@@ -71,7 +119,7 @@ export default function Form({day , setDay , month , setMonth , year , setYear} 
             <div className="mt-1 flex items-center">
                 <p className="h-[2px] w-full bg-Off-white"></p>
                 <button onClick={calculateAge} className="bg-Purple rounded-full p-5 hover:bg-Off-black">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="46" height="44" viewBox="0 0 46 44"><g fill="none" stroke="#FFF" stroke-width="2"><path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44"/></g></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="46" height="44" viewBox="0 0 46 44"><g fill="none" stroke="#FFF" strokeWidth="2"><path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44"/></g></svg>
                 </button>
             </div>
         </>
